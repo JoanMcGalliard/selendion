15997492fd95a615904adfa9f1a3f3a74b1dcab3
googlecode@mcgalliard.org <googlecode@mcgalliard.org@d9314cfe-8de7-11dd-a017-db29c870d619>
2008-10-04 10:09:14 +0000

    Joan - Working! Still need to add notExpectedToPass and #HREF handling

--- src/main/java/org/selendion/internal/command/RunSeleniumCommand.java
+++ src/main/java/org/selendion/internal/command/RunSeleniumCommand.java
@@ -12,12 +12,10 @@ import org.selendion.internal.util.SeleniumIdeReader;
 
 public class RunSeleniumCommand extends AbstractCommand {
 
-    private DocumentParser documentParser;
     private SeleniumIdeReader seleniumIdeReader;
 
 
-    public RunSeleniumCommand(DocumentParser documentParser, SeleniumIdeReader seleniumIdeReader) {
-        this.documentParser = documentParser;
+    public RunSeleniumCommand(SeleniumIdeReader seleniumIdeReader) {
         this.seleniumIdeReader = seleniumIdeReader;
 
     }
@@ -39,44 +37,44 @@ public class RunSeleniumCommand extends AbstractCommand {
 
     private class DefaultStrategy implements Strategy {
 
-        void DefaultStrategy() {
-        }
-
-
         public void execute(CommandCall commandCall, Evaluator evaluator, ResultRecorder resultRecorder) {
             CommandCallList childCommands = commandCall.getChildren();
 
             childCommands.setUp(evaluator, resultRecorder);
             String seleniumFile = commandCall.getResource().getRelativeResource(evaluator.evaluate(commandCall.getExpression()).toString()).getPath();
             evaluator.evaluate(commandCall.getExpression());
-
             try {
                 seleniumIdeReader.runSeleniumScript(seleniumFile, evaluator);
             } catch (Exception e) {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
-
-
             childCommands.execute(evaluator, resultRecorder);
             childCommands.verify(evaluator, resultRecorder);
         }
-
-
     }
-
 
     private class TableStrategy implements Strategy {
 
 
         public void execute(CommandCall commandCall, Evaluator evaluator, ResultRecorder resultRecorder) {
-               TableSupport tableSupport = new TableSupport(commandCall, documentParser);
-               Row[] detailRows = tableSupport.getDetailRows();
-               for (Row detailRow : detailRows) {
-                   commandCall.setElement(detailRow.getElement());
-                   commandCall.setChildren(tableSupport.getCommandCallsFor(detailRow, commandCall.getResource()));
-                   commandCall.execute(evaluator, resultRecorder);
-               }
-           }
+
+            TableSupport tableSupport = new TableSupport(commandCall);
+
+            Row[] detailRows = tableSupport.getDetailRows();
+
+            for (Row detailRow : detailRows) {
+                if (detailRow.getCells().length != tableSupport.getColumnCount()) {
+                    throw new RuntimeException("The <table> 'execute' command only supports rows with an equal number of columns.");
+                }
+                commandCall.setElement(detailRow.getElement());
+                tableSupport.copyCommandCallsTo(detailRow);
+                commandCall.execute(evaluator, resultRecorder);
+            }
+        }
+
 
     }
+
 }
+
+
