12a929708be1d0927e4af8df778f8c54d8e62afe
googlecode@mcgalliard.org <googlecode@mcgalliard.org@d9314cfe-8de7-11dd-a017-db29c870d619>
2008-10-08 11:26:23 +0000

    Now uses user-extensions

---
+++ trunk/spec/examples/suite/SetVarsTest.java
@@ -0,0 +1,11 @@
+/*
+	Copyright Joan McGalliard, 2008
+*/
+package examples.suite;
+
+import org.selendion.integration.concordion.SelendionTestCase;
+
+public class SetVarsTest extends SelendionTestCase {
+
+
+}
