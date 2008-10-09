88cc132eac71e13c8721b8fedca11cd6a091abf4
googlecode@mcgalliard.org <googlecode@mcgalliard.org@d9314cfe-8de7-11dd-a017-db29c870d619>
2008-10-09 19:28:17 +0000

    Suite is now working properly

--- trunk/src/main/java/org/selendion/internal/command/AddToSuiteCommand.java
+++ trunk/src/main/java/org/selendion/internal/command/AddToSuiteCommand.java
@@ -4,48 +4,95 @@ import org.concordion.internal.command.AbstractCommand;
 import org.concordion.internal.CommandCall;
 import org.concordion.api.Evaluator;
 import org.concordion.api.ResultRecorder;
+import org.concordion.api.Resource;
 import org.concordion.integration.junit3.ConcordionTestCase;
-import org.selendion.internal.command.pure_concordion.PureConcordionDemoTest;
-import org.selendion.internal.command.pure_concordion.PureConcordionSelendionDemoTest;
+import org.selendion.internal.util.TestDescription;
 
 
 import java.util.Vector;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 import junit.runner.TestCaseClassLoader;
+import nu.xom.Document;
+import nu.xom.Builder;
 
 
 public class AddToSuiteCommand extends AbstractCommand {
-    private Vector<Class<? extends ConcordionTestCase>> suite;
+    private Vector<TestDescription> suite;
 
-    public AddToSuiteCommand(Vector<Class<? extends ConcordionTestCase>> suite) {
+    public AddToSuiteCommand(Vector<TestDescription> suite) {
         this.suite = suite;
     }
 
     public void execute(CommandCall commandCall, Evaluator evaluator, ResultRecorder resultRecorder) {
-        String htmlFilename=commandCall.getResource().getRelativeResource(evaluator.evaluate(commandCall.getExpression()).toString()).getPath();
-
-      suite.add(getClass(htmlFilename));
-
-    }
-    private Class<? extends ConcordionTestCase> getClass (String htmlFilename) {
-                        String className = htmlFilename;
-
-              if (className.startsWith("/")) {
-                  className= className.substring(1,className.length());
-              }               if (className.endsWith(".html")) {
-                   className= className.substring(0,className.length()-5);
-               }
-               className = className.replace("/", ".");
-        System.out.println(className);
-       TestCaseClassLoader loader = new TestCaseClassLoader();
-        Class<? extends ConcordionTestCase> x =null;
-        try {
-            x = (Class<? extends ConcordionTestCase>)loader.loadClass(className+"Test");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        String htmlFilename = evaluator.evaluate(commandCall.getExpression()).toString();
+        String htmlResource=commandCall.getResource().getRelativeResource(htmlFilename)
+                .getPath().replaceFirst("^/", "");
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        URL url = contextClassLoader.getResource(htmlResource);
+        if (url == null) {
+            throw new RuntimeException(String.format("Can't add %s to suite: file does not exist.", htmlFilename));
         }
-        return x;
-
+        File f = new File(contextClassLoader.getResource(htmlResource).getPath());
+        if (f.isFile()) {
+            suite.add(new TestDescription(htmlFilename, getTitleOfPage(htmlFilename), getClass(htmlResource) ));
+        } else if (f.isDirectory()) {
+            walk(htmlFilename, htmlResource, commandCall.getResource());
+        }
     }
-            
+
+    private void walk(String htmlFilename, String htmlResource, Resource resource) {
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        File f = new File(contextClassLoader.getResource(htmlResource).getPath());
+        if (f.isFile() && f.toString().matches(".*\\.html")) {
+            System.out.println("Adding "+ htmlResource );
+            suite.add(new TestDescription(htmlFilename, getTitleOfPage(htmlResource), getClass(htmlResource) ));
+
+        }  else if (f.isDirectory()) {
+            for (String sub : f.list()) {
+                walk(htmlFilename.replaceFirst("/$", "") + "/" + sub, htmlResource.replaceFirst("/$", "") + "/" + sub, resource);
+            }
+        }
+    }
+    private Class<? extends ConcordionTestCase> getClass(String htmlResource) {
+        String className = htmlResource;
+
+        if (className.endsWith(".html")) {
+            className = className.substring(0, className.length() - 5);
+        }
+        className = className.replace("/", ".");
+        TestCaseClassLoader loader = new TestCaseClassLoader();
+        Class<? extends ConcordionTestCase> testClass = null;
+        try {
+            testClass = (Class<? extends ConcordionTestCase>) loader.loadClass(className + "Test");
+        } catch (ClassNotFoundException e) {
+            try {
+                testClass = (Class<? extends ConcordionTestCase>) loader.loadClass(className);
+            } catch (ClassNotFoundException e1) {
+                e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+        return testClass;
+    }
+    private String getTitleOfPage(String test)  {
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        String title;
+        try {
+            Document doc = new Builder().build(new InputStreamReader(contextClassLoader.getResourceAsStream(test)));
+             title = doc.query("//title").get(0).getValue();
+        } catch (Exception e) {
+            title="";
+        }
+
+        if (title.length() > 0) {
+            return title;
+        } else {
+            return test.replaceFirst(".*/", "");
+        }
+    }
+
+
+
 }
--- trunk/src/main/java/org/selendion/internal/command/RunSuiteCommand.java
+++ trunk/src/main/java/org/selendion/internal/command/RunSuiteCommand.java
@@ -4,122 +4,101 @@ import org.concordion.internal.command.AbstractCommand;
 import org.concordion.internal.CommandCall;
 import org.concordion.internal.util.Announcer;
 import org.concordion.api.*;
+import org.concordion.api.Element;
 import org.concordion.integration.junit3.ConcordionTestCase;
 import org.selendion.internal.RunSuiteListener;
+import org.selendion.internal.util.TestDescription;
+import org.selendion.internal.util.ActiveTestSuiteRestricted;
 import org.junit.Test;
+import org.xml.sax.helpers.XMLReaderFactory;
+import org.xml.sax.SAXException;
+import org.ccil.cowan.tagsoup.Parser;
 
 import java.util.Vector;
+import java.util.Set;
+import java.util.Enumeration;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 import junit.framework.TestSuite;
 import junit.framework.TestResult;
+import junit.framework.TestFailure;
 import junit.extensions.ActiveTestSuite;
+import junit.runner.TestCaseClassLoader;
+import nu.xom.*;
 
 
 public class RunSuiteCommand extends AbstractCommand {
     private Announcer<RunSuiteListener> listeners = Announcer.to(RunSuiteListener.class);
-    private Vector<Class<? extends ConcordionTestCase>> suite;
+    private Vector<TestDescription> suite;
 
 
-    public RunSuiteCommand(Vector<Class<? extends ConcordionTestCase>> suite) {
+    public RunSuiteCommand(Vector<TestDescription> suite) {
         this.suite = suite;
     }
 
     public void addRunSuiteListener(RunSuiteListener runSuiteListener) {
-		listeners.addListener(runSuiteListener);
-	}
+        listeners.addListener(runSuiteListener);
+    }
 
     public void execute(CommandCall commandCall, Evaluator evaluator, ResultRecorder resultRecorder) {
         TestSuite testSuite;
-        if (commandCall.getExpression().equals("active")) {
-            testSuite =new ActiveTestSuite();
-        } else {
-            testSuite =new TestSuite();
+        int threads = Integer.parseInt(evaluator.evaluate(commandCall.getExpression()).toString());
 
+        if (threads != 1 ) {
+            testSuite = new ActiveTestSuiteRestricted(6);
+        } else {
+            testSuite = new TestSuite();
         }
-        for (Class<? extends ConcordionTestCase> test : suite) {
-            testSuite.addTestSuite(test);
+        for (TestDescription test : suite) {
+            testSuite.addTestSuite(test.getClazz());
         }
-        TestResult testResult=new TestResult();
+        TestResult testResult = new TestResult();
         testSuite.run(testResult);
 
-
-        String result=commandCall.getExpression();
-        Element element = commandCall.getElement();
-        if (result.equals("pass"))  {
-
-
-            resultRecorder.record(Result.SUCCESS);
-            announceSuccess(element);
-
-        }   else if (result.equals("fail"))  {
-             resultRecorder.record(Result.FAILURE);
-            announceFailure(element);
-
-        }   else if (result.equals("exception"))  {
-            throw new RuntimeException ("Blah!");
-
+        Vector<Class> failures = new Vector();
+        Enumeration<TestFailure> errors = testResult.errors();
+        while (errors.hasMoreElements()) {
+            failures.add(errors.nextElement().failedTest().getClass());
         }
-        
-//
-
-
+        Element element = commandCall.getElement();
+        if (testResult.wasSuccessful()) {
+            announceSuccess(element);
+        } else {
+            announceFailure(element);
+        }
+        Element list;
+        if (threads != 1) {
+            list = new Element("ul");
+        } else {
+            list = new Element("ol");
+        }
+        for (TestDescription test : suite) {
+            Element testElement = new Element("a");
+            testElement.addAttribute("href", test.getFile());
+            testElement.appendText(test.getTitle());
+            if (failures.contains(test.getClazz())) {
+                resultRecorder.record(Result.FAILURE);
+                announceFailure(testElement);
+            } else {
+                resultRecorder.record(Result.SUCCESS);
+                announceSuccess(testElement);
+            }
+            Element li = new Element("li");
+            li.appendChild(testElement);
+            list.appendChild(li);
+        }
+        Element parent = element.getParent();
+        parent.appendChild(list);
     }
 
     private void announceSuccess(Element element) {
         listeners.announce().successReported(new RunSuiteSuccessEvent(element));
     }
 
-	private void announceFailure(Element element) {
-		listeners.announce().failureReported(new RunSuiteFailureEvent(element));
-	}
-}
-
-
-
-
-          /*
-
-    private Announcer<AssertEqualsListener> listeners = Announcer.to(AssertEqualsListener.class);
-    private final Comparator<Object> comparator;
-
-    public AssertEqualsCommand() {
-        this(new BrowserStyleWhitespaceComparator());
-    }
-
-    public AssertEqualsCommand(Comparator<Object> comparator) {
-        this.comparator = comparator;
-    }
-
-    public void addAssertEqualsListener(AssertEqualsListener listener) {
-        listeners.addListener(listener);
-    }
-
-    public void removeAssertEqualsListener(AssertEqualsListener listener) {
-        listeners.removeListener(listener);
-    }
-
-    @Override
-    public void verify(CommandCall commandCall, Evaluator evaluator, ResultRecorder resultRecorder) {
-        Check.isFalse(commandCall.hasChildCommands(), "Nesting commands inside an 'assertEquals' is not supported");
-
-        Element element = commandCall.getElement();
-
-        Object actual = evaluator.evaluate(commandCall.getExpression());
-        String expected = element.getText();
-
-        if (comparator.compare(actual, expected) == 0) {
-
-        } else {
-
-        }
-    }
-
-    private void announceSuccess(Element element) {
-        listeners.announce().successReported(new AssertEqualsSuccessEvent(element));
-    }
-
-    private void announceFailure(Element element, String expected, Object actual) {
-        listeners.announce().failureReported(new AssertEqualsFailureEvent(element, expected, actual));
+    private void announceFailure(Element element) {
+        listeners.announce().failureReported(new RunSuiteFailureEvent(element));
     }
 }
-*/
