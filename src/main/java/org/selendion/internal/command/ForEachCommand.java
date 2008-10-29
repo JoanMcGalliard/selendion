2a47e31f7f2faca6614c4bf694b2d7aa1f75216a
googlecode@mcgalliard.org <googlecode@mcgalliard.org@d9314cfe-8de7-11dd-a017-db29c870d619>
2008-10-29 21:03:47 +0000

    starting on foreach

---
+++ src/main/java/org/selendion/internal/command/ForEachCommand.java
@@ -0,0 +1,15 @@
+/*
+        Copyright Joan McGalliard, 2008
+*/
+
+package org.selendion.internal.command;
+
+import org.concordion.internal.command.AbstractCommand;
+
+public class ForEachCommand extends AbstractCommand {
+
+    public ForEachCommand() {
+
+    }
+
+}
