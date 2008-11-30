76447edd235f13f0a0a6e597d8ecf0fbd79d58bd
googlecode@mcgalliard.org <googlecode@mcgalliard.org@d9314cfe-8de7-11dd-a017-db29c870d619>
2008-11-30 14:45:38 +0000

    Pass system properties to app

--- src/main/java/org/selendion/internal/SelendionEvaluator.java
+++ src/main/java/org/selendion/internal/SelendionEvaluator.java
@@ -4,10 +4,20 @@ import org.concordion.internal.SimpleEvaluator;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Properties;
+import java.util.Enumeration;
 
 public class SelendionEvaluator extends SimpleEvaluator {
+    private static String PREFIX = "selendion.";
     public SelendionEvaluator(Object o) {
         super(o);
+        Enumeration<?> propertyNames = System.getProperties().propertyNames();
+        while (propertyNames.hasMoreElements()) {
+            String name = (String) propertyNames.nextElement();
+            if (name.startsWith(PREFIX)) {
+                setVariable("#" + name.replaceFirst(PREFIX, ""), System.getProperty(name));
+            }
+        }
     }
 
     public Object evaluate(String expression) {
