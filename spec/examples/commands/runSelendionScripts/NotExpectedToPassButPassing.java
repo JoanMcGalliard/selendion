3361857313c0c860104cec612c33b48dc971fead
googlecode@mcgalliard.org <googlecode@mcgalliard.org@d9314cfe-8de7-11dd-a017-db29c870d619>
2008-12-08 17:01:56 +0000

    report notExpectedToPass when executing runSelendion

---
+++ spec/examples/commands/runSelendionScripts/NotExpectedToPassButPassing.java
@@ -0,0 +1,10 @@
+package examples.commands.runSelendionScripts;
+
+import org.selendion.integration.concordion.SelendionTestCase;
+
+public class NotExpectedToPassButPassing extends SelendionTestCase {
+    public String greetingFor(String firstName) {
+        return String.format("Hello %s!", firstName);
+    }
+    
+}
