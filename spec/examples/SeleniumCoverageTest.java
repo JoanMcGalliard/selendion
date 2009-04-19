d7c3b851dc40d081924e9407a873c06ad6e9be39
googlecode@mcgalliard.org <googlecode@mcgalliard.org@d9314cfe-8de7-11dd-a017-db29c870d619>
2009-04-19 08:54:53 +0000

    amalgamating tests

--- spec/examples/SeleniumCoverageTest.java
+++
@@ -1,9 +0,0 @@
-/*
-	Copyright Joan McGalliard, 2008-9
-*/
-package examples;
-
-import org.selendion.integration.concordion.SelendionTestCase;
-
-public class SeleniumCoverageTest extends SelendionTestCase {
-}
