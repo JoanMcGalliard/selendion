2017f0fc7f4de37e536559da2f2596184dad33b9
googlecode@mcgalliard.org <googlecode@mcgalliard.org@d9314cfe-8de7-11dd-a017-db29c870d619>
2009-04-25 00:16:49 +0000

    modifying test structure

---
+++ spec/selendion/commands/Suite/AddToSuiteTest.java
@@ -0,0 +1,11 @@
+package selendion.commands.Suite;
+
+
+import java.util.ArrayList;
+
+public class AddToSuiteTest extends SuiteTest {
+    public ArrayList getList() {
+        return  new java.util.ArrayList();
+    }
+    
+}
---
+++ spec/selendion/commands/Suite/RunSuiteTest.java
@@ -0,0 +1,5 @@
+package selendion.commands.Suite;
+
+
+public class RunSuiteTest extends SuiteTest {
+}
