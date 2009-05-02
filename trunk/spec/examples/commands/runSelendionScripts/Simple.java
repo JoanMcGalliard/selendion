bca380c64ba08b3e532fd96652de99cd9c69bcc7
googlecode@mcgalliard.org <googlecode@mcgalliard.org@d9314cfe-8de7-11dd-a017-db29c870d619>
2009-05-02 10:17:53 +0000

    improve tests.  Tweak to htmlunit driver

--- trunk/spec/examples/commands/runSelendionScripts/Simple.java
+++ trunk/spec/examples/commands/runSelendionScripts/Simple.java
@@ -7,5 +7,8 @@ package examples.commands.runSelendionScripts;
 import org.selendion.integration.concordion.SelendionTestCase;
 
 public class Simple extends SelendionTestCase {
+    public String getValue() {
+        return "value";
+    }
 
 }
