2118f2c69520600b801eb896821f488a5ea94134
googlecode@mcgalliard.org <googlecode@mcgalliard.org@d9314cfe-8de7-11dd-a017-db29c870d619>
2013-03-01 16:35:15 +0000

    Committing latest build before release

--- trunk/selendion-example/acceptance-tests/Suite.java
+++
@@ -1,4 +0,0 @@
-import org.selendion.integration.concordion.SelendionTestCase;
-
-public class Suite extends SelendionTestCase{
-}
--- trunk/selendion-example/acceptance-tests/tests/google/GoogleWebTitleTest.java
+++
@@ -1,6 +0,0 @@
-package tests.google;
-
-import org.selendion.integration.concordion.SelendionTestCase;
-
-public class GoogleWebTitleTest extends SelendionTestCase {
-}
